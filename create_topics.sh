8e9f3f9fd6d8e5eaee46f6bb5a13c36399f34cb9
Kiran Kumar H.M <kirankumarhm2004@gmail.com>
2023-10-05 15:25:30 +0530

    Update create_topics.sh

--- create_topics.sh
+++ create_topics.sh
@@ -1,11 +1,4 @@
 
-docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic shopify-collection-create  --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
-docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic shopify-product-create  --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
-docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic shopify-order-create  --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
-docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic shopify-order-transaction-create   --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
-docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic shopify-order-fulfilled-create   --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
-docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic shopify-fulfillments-create  --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
-docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic shopify-billinga-attempts  --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
-docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic shopify-checkout-create  --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
-docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic myTopic  --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
+docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic checkout-create  --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
+docker-compose -f kafka-docker-compose.yml exec broker0 kafka-topics --create --topic mytopic  --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server broker0:9092,broker1:9093
 
